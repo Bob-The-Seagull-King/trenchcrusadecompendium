d156734d71619b19b5761d913f219fe9ea01a7e7
Bob-The-Seagull-King <laneedwardsbrown@gmail.com>
2024-06-01 16:17:56 +1200

    display object change from hover to popup

--- src/display/components/subcomponents/informationpanel/hovermisc/EquipmentHover.tsx
+++ src/display/components/subcomponents/informationpanel/hovermisc/EquipmentHover.tsx
@@ -1,29 +1,33 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import '../../../../../resources/styles/_icon.scss'
-import React from 'react'
+import React, { useState } from 'react'
 
 import { PlayerEquipment } from '../../../../../classes/feature/equipment/Equipment'
 import * as HoverCard from '@radix-ui/react-hover-card';
 import EquipmentDisplay from '../../../../../display/components/features/equipment/EquipmentDisplay';
+import Modal from 'react-bootstrap/Modal';
 
 const GlossaryHover = (props: any) => {
     const ruleObject: PlayerEquipment = props.data
     const ruleName = props.titlename
 
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     return (
-      <HoverCard.Root>
-        <HoverCard.Trigger asChild>
-          <span className='glossaryPurple hovermouse'>{ruleName}</span>
-        </HoverCard.Trigger>
-        <HoverCard.Portal>
-          <HoverCard.Content className="HoverCardContent" sideOffset={5}>
-              <div  className='popupItemBody'>
-                <EquipmentDisplay data={ruleObject}/>
-              </div>
-            <HoverCard.Arrow className="HoverCardArrow" />
-          </HoverCard.Content>
-        </HoverCard.Portal>
-      </HoverCard.Root>
+      <>
+          <span>
+            <span className='glossaryPurple hovermouse' onClick={() => handleShow()}>{ruleName}</span>                
+          </span>
+
+          <Modal onEnterKeyDown={() => handleClose()} show={show} size="lg" contentClassName="" dialogClassName=""  onHide={handleClose} keyboard={true}  centered>
+              <Modal.Body > 
+                  <EquipmentDisplay data={ruleObject}/> 
+              </Modal.Body>
+          </Modal>
+      </>
     )
 }
 
--- src/display/components/subcomponents/informationpanel/hovermisc/ModelHover.tsx
+++ src/display/components/subcomponents/informationpanel/hovermisc/ModelHover.tsx
@@ -1,29 +1,34 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import '../../../../../resources/styles/_icon.scss'
-import React from 'react'
+import React, { useState } from 'react'
 
 import { PlayerModel } from '../../../../../classes/feature/models/Model'
 import * as HoverCard from '@radix-ui/react-hover-card';
 import ModelDisplay from '../../../../../display/components/features/models/ModelDisplay';
+import Modal from 'react-bootstrap/Modal';
 
 const ModelHover = (props: any) => {
     const ruleObject: PlayerModel = props.data
     const ruleName = props.titlename
 
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     return (
-      <HoverCard.Root>
-        <HoverCard.Trigger asChild>
-          <span className='glossaryPurple hovermouse'>{ruleName}</span>
-        </HoverCard.Trigger>
-        <HoverCard.Portal>
-          <HoverCard.Content className="HoverCardContent" sideOffset={5}>
-              <div  className='popupItemBody'>
-                <ModelDisplay data={ruleObject}/>
-              </div>
-            <HoverCard.Arrow className="HoverCardArrow" />
-          </HoverCard.Content>
-        </HoverCard.Portal>
-      </HoverCard.Root>
+      <>
+          <span>
+            <span className='glossaryPurple hovermouse' onClick={() => handleShow()}>{ruleName}</span>                
+          </span>
+
+          <Modal onEnterKeyDown={() => handleClose()} show={show} size="lg" contentClassName="" dialogClassName=""  onHide={handleClose} keyboard={true}  centered>
+              <Modal.Body > 
+                  <ModelDisplay data={ruleObject}/>
+              </Modal.Body>
+          </Modal>
+      </>
+      
     )
 }
 
