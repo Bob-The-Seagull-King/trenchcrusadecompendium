c9efcf943300a1ac8b42110e6368a6533f8457cf
Bob-The-Seagull-King <laneedwardsbrown@gmail.com>
2024-07-11 10:16:55 +1200

    Display tag glossary rules

--- src/display/components/subcomponents/TagDisplay.tsx
+++ src/display/components/subcomponents/TagDisplay.tsx
@@ -4,14 +4,32 @@ import React from 'react'
 
 import {capitalizeString} from '../../../utility/functions'
 import {ITrenchCrusadeItemTag} from '../../../classes/TrenchCrusadeItem'
+import GlossaryHover from './glossary/GlossaryHover'
+import { GlossaryRule, IGlossaryRule } from '../../../classes/feature/glossary/Glossary'
+import { Requester } from '../../../factories/Requester'
 
 const TagDisplay = (props: any) => {
     const tag: ITrenchCrusadeItemTag = props.data
+    const name = capitalizeString(tag.tag_name.toString() || "")
+    const val =  capitalizeString(tag.val.toString() || "")
+    let GlossaryObject = null;
+    const searchResult = Requester.MakeRequest( {searchtype: "id", searchparam: {type: "glossary", id: tag.val}} )
+    if (JSON.stringify(searchResult) != "{}") {
+        const GlossaryData: IGlossaryRule = searchResult as IGlossaryRule
+        GlossaryObject = new GlossaryRule(GlossaryData)
+    }
 
     return (
+        <>
+        {JSON.stringify(searchResult) == "{}" &&
         <div className="tagItem tagText">
-            &#x2b9e; {capitalizeString(tag.tag_name.toString() || "")} {capitalizeString(tag.val.toString() || "")}
-        </div>
+            &#x2b9e; {name} {val}
+        </div>}
+        {JSON.stringify(searchResult) != "{}" &&
+        <div className="tagItem tagText">
+            <div style={{display:"flex"}}>&#x2b9e;<GlossaryHover data={GlossaryObject} titlename={name}/></div>
+        </div>}
+        </>
     )
 }
 
