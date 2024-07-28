3364f4fbe78cb412fa285a15deb16f03b7f5e971
Bob-The-Seagull-King <laneedwardsbrown@gmail.com>
2024-07-29 09:37:06 +1200

    Comments PART_05 glossary

--- src/classes/feature/glossary/Glossary.ts
+++ src/classes/feature/glossary/Glossary.ts
@@ -1,19 +1,20 @@
-import {ITrenchCrusadeItemData, TrenchCrusadeItem} from '../../TrenchCrusadeItem'
-import {ItemType} from '../../Enum'
+import { ITrenchCrusadeItemData, TrenchCrusadeItem } from '../../TrenchCrusadeItem'
+import { ItemType } from '../../Enum'
 import { DescriptionFactory } from '../../../utility/functions';
 
+/**
+ * Description data for glossary rule
+ */
 interface IGlossaryRule extends ITrenchCrusadeItemData {
     description: []
 }
 
+/**
+ * Rule description used as a glossary term.
+ */
 class GlossaryRule extends TrenchCrusadeItem {
     public readonly Description;
-    
-    /**
-     * Assigns parameters and creates a series of description
-     * objects with DescriptionFactory
-     * @param data Object data in IPlayerModel format
-     */
+
     public constructor(data: IGlossaryRule)
     {
         super(data)
